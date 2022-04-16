4496de3e51f5f6342f327d9985c12941af35da84
SRINIVAS <srinivasshanti10@gmail.com>
2022-04-16 19:24:53 +0530

    project

---
+++ app/src/main/java/com/example/librarymangementsystem/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.librarymangementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_xml);
+    }
+}
